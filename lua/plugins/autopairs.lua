38b61bee701523ee341b1f90312120a054fadee0
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-16 19:37:34 -0600

    fix config

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -7,9 +7,9 @@ return {
 			npairs.setup({
 				check_ts = true,
 				ts_config = {
-					lua = { "string" }, -- it will not add a pair on that treesitter node
+					lua = { "string" },
 					javascript = { "template_string" },
-					java = false, -- don't check treesitter on java
+					java = false,
 				},
 			})
 			local ts_conds = require("nvim-autopairs.ts-conds")
