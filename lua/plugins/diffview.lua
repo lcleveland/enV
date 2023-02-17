ed510900cdeea4fc379dfcd6f7235a2b747d859b
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-17 09:21:32 -0600

    add diffview

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"sindrets/diffview.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		cmd = {
+			"DiffviewLog",
+			"DiffviewOpen",
+			"DiffviewClose",
+			"DiffviewRefresh",
+			"DiffviewFocusFiles",
+			"DiffviewFileHistory",
+			"DiffviewToggleFiles",
+		},
+	},
+}
