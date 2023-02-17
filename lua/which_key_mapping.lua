7a4f8b861f94cd2b19786106fcaa188b92802d03
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-17 09:23:49 -0600

    add mapping

--- lua/which_key_mapping.lua
+++ lua/which_key_mapping.lua
@@ -4,4 +4,8 @@ wk.register({
 		name = "file",
 		f = { "<cmd>Telescope find_files<cr>", "Find File" },
 	},
+	g = {
+		name = "git",
+		d = { "<cmd>DiffviewOpen<cr>", "Diffview" },
+	},
 }, { prefix = "<leader>" })
