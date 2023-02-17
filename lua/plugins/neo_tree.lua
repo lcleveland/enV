d454ba352dea70a58fc705bacf071da54c2e39d0
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-17 17:59:46 -0600

    add neo_tree plugin

---
+++ lua/plugins/neo_tree.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		cmd = "Neotree",
+		config = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+	},
+}
