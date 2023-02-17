d521feba43acc3bc203ac34d08dac1addb17f1e8
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-17 13:34:08 -0600

    fix colorscheme during startup

--- init.lua
+++ init.lua
@@ -2,9 +2,13 @@ require("keymaps")
 require("general_settings")
 require("bootstrap")
 require("lazy").setup("plugins", {
-	options = {
-		install = {
-			colorscheme = { "moonbow" },
-		},
+	ui = {
+		border = "rounded",
+	},
+	install = {
+		colorscheme = { "moonbow" },
+	},
+	checker = {
+		enabled = true,
 	},
 })
--- lua/plugins/moonbow.lua
+++ lua/plugins/moonbow.lua
@@ -2,12 +2,9 @@ return {
 	{
 		"arturgoms/moonbow.nvim",
 		priority = 1000,
-		lazy = false,
 		config = function()
-			vim.cmd([[colorscheme moonbow]])
-		end,
-		init = function()
 			vim.opt.termguicolors = true
+			vim.cmd([[colorscheme moonbow]])
 		end,
 	},
 }
