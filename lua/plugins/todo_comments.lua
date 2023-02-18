601e43f78b31ab232a60b4979ace59cd53394b12
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-17 19:32:25 -0600

    add todo

---
+++ lua/plugins/todo_comments.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"folke/todo-comments.nvim",
+		event = "VeryLazy",
+		config = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"folke/trouble.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+}
