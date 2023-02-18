53cfc434fe7cdc9376e3a0104ccc75d05b1997a0
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-18 12:30:32 -0600

    add some telescope key maps

--- lua/which_key_mapping.lua
+++ lua/which_key_mapping.lua
@@ -11,6 +11,17 @@ wk.register({
 		d = { "<cmd>DiffviewOpen<cr>", "Diffview" },
 		p = { "<cmd>Git push<cr>", "Push" },
 	},
+	t = {
+		name = "[t]elescope",
+		f = { "<cmd>Telescope find_files<cr>", "Files" },
+		g = {
+			name = "[g]it",
+			b = { "<cmd>Telescope git_branches<cr>", "Branches" },
+			c = { "<cmd>Telescope git_commits<cr>", "Commits" },
+			f = { "<cmd>Telescope git_files<cr>", "Files" },
+			s = { "<cmd>Telescope git_status<cr>", "Status" },
+		},
+	},
 	L = { "<cmd>Lazy<cr>", "[L]azy" },
 	M = { "<cmd>Mason<cr>", "[M]ason" },
 	N = { "<cmd>Neotree<cr>", "[N]eotree" },
